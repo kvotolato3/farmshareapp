c85a0e67a5ee5057ba5fbfb399c8d685ec70f0fa
Kathleen Votolato <kvotolato3@gmail.com>
2014-07-03 10:52:03 -0400

    setup devise for user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
+  root 'signup#index'
   # root 'welcome#index'
 
   # Example of regular route:
