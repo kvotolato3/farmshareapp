a2b1c5ea9129dbeab8643bf5fa238ca4164bf911
Kathleen Votolato <kvotolato3@gmail.com>
2014-07-03 15:39:54 -0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,30 @@
+ShareOption.delete_all
+
+ShareOption.create(name: "Summer Vegetable Share",
+  description: nil,
+  image_url: nil,
+  price: 570.00,
+  start_date: "2014-06-15 [09:00:00]",
+  end_date: "2014-10-15 [09:00:00]",
+  is_season: true,
+  num_of_distributions: 20)
+
+ShareOption.create(name: "Summer and Fall Vegetable Share",
+  description: nil,
+  image_url: nil,
+  price: 705.00,
+  start_date: "2014-06-15 [09:00:00]",
+  end_date: "2014-12-15 [09:00:00]",
+  is_season: true,
+  num_of_distributions: 24)
+
+ShareOption.create(name: "Fruit Shares",
+  description: "A deluxe option including locally grown fruit for the Summer season.
+  It begins in June with Strawberries and continues with Blueberries, Plums, Peaches, Melons
+  and a wide variety of Apples in the fall. Sourced from local orchards that use low spray
+  Integrated Pest Management. Some fruit will be organic from Red Fire. Must order a vegetable
+  share to get fruit.",
+  image_url: nil,
+  price: 195.00,
+  num_of_distributions: 20)
 
