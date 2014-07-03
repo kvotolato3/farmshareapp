c191deefbe37bfedb529e8728bf9068a34057932
Kathleen Votolato <kvotolato3@gmail.com>
2014-07-03 16:23:44 -0400

    Add more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,5 +26,37 @@ ShareOption.create(name: "Fruit Shares",
   share to get fruit.",
   image_url: nil,
   price: 195.00,
+  start_date: "2014-06-15 [09:00:00]",
+  end_date: "2014-10-15 [09:00:00]",
   num_of_distributions: 20)
+
+ShareOption.create(name: "Flower Shares",
+  description: "Enjoy a season of fresh local flowers! Organically-grown. A weekly bouquet of flowers picked
+  fresh on our farm.",
+  price: 100.00,
+  start_date: "2014-07-15 [09:00:00]",
+  end_date: "2014-09-15 [09:00:00]",
+  num_of_distributions: 10)
+
+ShareOption.create(name: "Egg Shares",
+  description: "Delicious local eggs! Pasture-raised hens graze in fields and eat organic grain. Eggs
+  are available with Summer and Fall seasons. Sourced from local egg farmers.",
+  price: 6.50,
+  start_date: "2014-07-15 [09:00:00]",
+  end_date: "2014-12-15 [09:00:00]",
+  num_of_distributions: 24)
+
+
+ShareOption.create(name: "Home Delivery",
+  description: "An excellent deal for convenient and green delivery. Have the hard-core bikers at Metro
+  Pedal Power deliver your share direct to you. Tricycle Delivery to Cambridge, Somerville, and parts of
+  Boston. Includes delivery of any add-on shares you get too!",
+  price: 200.00,
+  start_date: "2014-07-15 [09:00:00]",
+  end_date: "2014-12-15 [09:00:00]",
+  num_of_distributions: 24)
+
+
+
+
 
