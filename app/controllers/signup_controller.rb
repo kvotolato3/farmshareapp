ed7166c9b5bcc763684b81246e51b37921e27452
Kathleen Votolato <kvotolato3@gmail.com>
2014-07-03 16:23:22 -0400

    User can view extra options
    - Test pass

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,5 +1,6 @@
 class SignupController < ApplicationController
   def index
     @SeasonalShareOptions = ShareOption.where(is_season: true)
+    @ExtraShareOptions = ShareOption.where(is_season: false)
   end
 end
--- spec/features/user_views_signup_form_spec.rb
+++ spec/features/user_views_signup_form_spec.rb
@@ -33,3 +33,32 @@ feature 'User views seasons' do
   end
 end
 
+feature 'User views extra options' do
+  scenario 'successfully' do
+
+   fruit =  ShareOption.create(name: "Fruit Shares",
+      description: "a deluxe option",
+      price: 195.00,
+      start_date: "2014-06-15 [09:00:00]",
+      end_date: "2014-10-15 [09:00:00]",
+      num_of_distributions: 20)
+
+    flower = ShareOption.create(name: "Flower Shares",
+      description: "fresh flowers!",
+      price: 100.00,
+      start_date: "2014-07-15 [09:00:00]",
+      end_date: "2014-09-15 [09:00:00]",
+      num_of_distributions: 10)
+
+    visit root_path
+      expect(page).to have_content
+      fruit.name &&
+      fruit.price &&
+      fruit.num_of_distributions &&
+      flower.name &&
+      flower.price &&
+      flower.num_of_distributions
+      save_and_open_page
+  end
+end
+
