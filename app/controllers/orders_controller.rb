3b5153257743974095ab9246adb246b9fc120f50
Kathleen Votolato <kvotolato3@gmail.com>
2014-07-05 16:59:54 -0400

    Add autocreation of cart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,26 @@
 class OrdersController < ApplicationController
+before_action :set_order, only: [:show, :edit, :update, :destroy]
 
-def new
+def show
+end
+
+def edit
   @SeasonalShareOptions = ShareOption.where(is_season: true)
   @ExtraShareOptions = ShareOption.where(is_season: false)
+end
+
+def update
+end
+
+def destroy
+end
+
+def new
+  @order = Order.create
+  redirect_to edit_order_path(@order)
+end
+
+def create
 end
 
 private
