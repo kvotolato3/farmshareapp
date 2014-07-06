60b89cf1e578e21ffadb33bc93b2ecf97d101f4a
Kathleen Votolato <kvotolato3@gmail.com>
2014-07-06 19:45:04 -0400

    Add initial javascript for save functionality

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -1,3 +1,23 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).ready(function(){
+  $('#submit-button').click(CSASignup.saveOrder);
+});
+
+var CSASignup = CSASignup || {};
+
+CSASignup.saveOrder = function() {
+  var seasonalOptionId = $('input:checked[name=seasonal_share]').val();
+  var extraOptionsCollection = $('input:checked[name=extra_share]');
+  var extraOptionsArray = [];
+  extraOptionsCollection.each(function(i){extraOptionsArray[i] = $(this).val();
+  });
+
+
+  $.ajax({
+      url: '/order/' +,
+      type: 'POST',
+      data: {"seasonal_id": seasonalOptionId,
+      "extra_ids": extraOptionsArray}
+    });
+};
+
+
--- app/controllers/order_lines_controller.rb
+++ app/controllers/order_lines_controller.rb
@@ -1,2 +1,10 @@
 class OrderLinesController < ApplicationController
+
+  def create
+    seasonal_id = params["seasonal_id"]
+    extra_ids = params["extra_ids"]
+    order_id = params["id"]
+  end
 end
+
+
