95ae30f8b0aa80a4b50cd402457a2239cbf9bfc5
Kathleen Votolato <kvotolato3@gmail.com>
2014-07-07 17:56:38 -0400

    Wrote a model method to produce the time range
    for each pickup location.

--- app/models/pickup_location.rb
+++ app/models/pickup_location.rb
@@ -1,3 +1,30 @@
 class PickupLocation < ActiveRecord::Base
   belongs_to :order
+
+  def time_range
+    start = time_to_string(self.start_time)
+    finish = time_to_string(self.end_time)
+    return start << " to " << finish
+  end
+
+  def time_to_string(raw_time)
+    if raw_time > 1200
+      time = (raw_time-1200)
+        if time > 959
+          time = time.to_s
+          return time[0..1] << ":" << time[2..3] << " p.m."
+        else
+          time = time.to_s
+          return time[0] << ":" << time[1..2] << " p.m."
+        end
+    else
+      if raw_time > 959
+        time = raw_time.to_s
+        return time[0..1] << ":" << time[2..3] << " a.m."
+      else
+        time = time.to_s
+        return time[0] << ":" << time[1..2] << " a.m."
+      end
+    end
+  end
 end
