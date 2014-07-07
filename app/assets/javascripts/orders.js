973902ef1e07c6b1452a275b2a7541cd49bd6719
Kathleen Votolato <kvotolato3@gmail.com>
2014-07-07 10:43:38 -0400

    Add hidden field containing the order id to
    the signup page

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -8,15 +8,17 @@ CSASignup.saveOrder = function() {
   var seasonalOptionId = $('input:checked[name=seasonal_share]').val();
   var extraOptionsCollection = $('input:checked[name=extra_share]');
   var extraOptionsArray = [];
+  var orderId = 2;
   extraOptionsCollection.each(function(i){extraOptionsArray[i] = $(this).val();
   });
 
 
   $.ajax({
-      url: '/order/' +,
+      url: '/order_lines/',
       type: 'POST',
       data: {"seasonal_id": seasonalOptionId,
-      "extra_ids": extraOptionsArray}
+      "extra_ids": extraOptionsArray,
+      "order_id": orderId}
     });
 };
 
