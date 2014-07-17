36fcc6f3816bf1aae3b0e3f0c73f9ea9879b4bb5
Kathleen Votolato <kvotolato3@gmail.com>
2014-07-17 13:17:02 -0400

    Add if stmnt to Save button so it cannot be
    saved unless all inputs are valid.

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -27,51 +27,55 @@ CSASignup.displayShareOptions = function(options) {
   };
 
 CSASignup.saveOrder = function() {
-  alert("Valid: " + form.valid());
-  var seasonalOptionId = $('input:checked[name=seasonal_share]').val();
-  var extraOptionsCollection = $('input:checked[name=extra_share]');
-  var extraOptionsArray = [];
-  var orderId = $('input[name=order_id]').val();
-  var seasonalPrice = $('input[name=seasonal_price_' +seasonalOptionId +']').val();
-  var seasonalQuantity = $('input[name=seasonal_quantity_' +seasonalOptionId +']').val();
-  var pickupId = $('input:checked[name=pickup_location]').val();
-  var paymentPlan = $('#payment_plan option:selected').val();
-  var firstName = $('input[name=firstname]').val();
-  var lastName = $('input[name=lastname]').val();
-  var addressOne = $('input[name=address_1]').val();
-  var addressTwo = $('input[name=address_2]').val();
-  var city = $('input[name=city]').val();
-  var state = $('input[name=state]').val();
-  var zip = $('input[name=zip]').val();
-  var phone = $('input[name=phone]').val();
-  var email = $('input[name=email]').val();
+  if (form.valid()=== true) {
+    var seasonalOptionId = $('input:checked[name=seasonal_share]').val();
+    var extraOptionsCollection = $('input:checked[name=extra_share]');
+    var extraOptionsArray = [];
+    var orderId = $('input[name=order_id]').val();
+    var seasonalPrice = $('input[name=seasonal_price_' +seasonalOptionId +']').val();
+    var seasonalQuantity = $('input[name=seasonal_quantity_' +seasonalOptionId +']').val();
+    var pickupId = $('input:checked[name=pickup_location]').val();
+    var paymentPlan = $('#payment_plan option:selected').val();
+    var firstName = $('input[name=firstname]').val();
+    var lastName = $('input[name=lastname]').val();
+    var addressOne = $('input[name=address_1]').val();
+    var addressTwo = $('input[name=address_2]').val();
+    var city = $('input[name=city]').val();
+    var state = $('input[name=state]').val();
+    var zip = $('input[name=zip]').val();
+    var phone = $('input[name=phone]').val();
+    var email = $('input[name=email]').val();
 
-  extraOptionsCollection.each(function(){
-    var id = $(this).val();
-    var quantity = $('#' + id +' :input[name=quantity]').val();
-    var pair = new ExtraOption(id, quantity);
-    extraOptionsArray.push(pair);
-  });
+    extraOptionsCollection.each(function(){
+      var id = $(this).val();
+      var quantity = $('#' + id +' :input[name=quantity]').val();
+      var pair = new ExtraOption(id, quantity);
+      extraOptionsArray.push(pair);
+    });
 
-$.ajax({
-    url: '/orders/' + orderId,
-      type: 'PATCH',
-      data: {"order" : {"pickup_location_id": pickupId,
-            "payment_plan":paymentPlan,
-            "first_name":firstName,
-            "last_name":lastName,
-            "address_1":addressOne,
-            "address_2":addressTwo,
-            "city":city,
-            "state":state,
-            "zip":zip,
-            "phone":phone,
-            "email":email},
-            "order_lines": {"seasonal_option": {"id":seasonalOptionId, "price":seasonalPrice, "quantity": seasonalQuantity},
-            "extra_options": extraOptionsArray}
-      },
-      dataType: 'json'
+    $.ajax({
+      url: '/orders/' + orderId,
+        type: 'PATCH',
+        data: {"order" : {"pickup_location_id": pickupId,
+              "payment_plan":paymentPlan,
+              "first_name":firstName,
+              "last_name":lastName,
+              "address_1":addressOne,
+              "address_2":addressTwo,
+              "city":city,
+              "state":state,
+              "zip":zip,
+              "phone":phone,
+              "email":email},
+              "order_lines": {"seasonal_option": {"id":seasonalOptionId, "price":seasonalPrice, "quantity": seasonalQuantity},
+              "extra_options": extraOptionsArray}
+        },
+        dataType: 'json'
     }).done(CSASignup.reviewOrder);
+  }
+  else {
+    alert('Please correct highlighted fields');
+  }
 };
 
 CSASignup.reviewOrder = function(result) {
