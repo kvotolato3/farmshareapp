185676ca8d7ab6fc3bb0d8c3952b5a175b8d71ea
Kathleen Votolato <kvotolato3@gmail.com>
2016-06-22 13:24:30 -0400

    Removed first name and last name back end validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,6 @@
 class Order < ActiveRecord::Base
   has_many :order_lines
 
-  validates :first_name, :last_name, presence: true
-
   def full_name
     self.first_name + " " + self.last_name
   end
